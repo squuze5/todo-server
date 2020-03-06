3ce27849ce0f151b3df224b00a3386c8bc1cf133
nikolaskokhno <kokhno.mm@gmail.com>
2020-03-06 19:34:03 +0200

    Create route get all todo / Create route to created new todo

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,44 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+admin.initializeApp();
+
+const express = require('express');
+const app = express();
+
+app.get('/todo', (req, res) => {
+    admin
+        .firestore()
+        .collection('todo')
+        .get()
+        .then(data => {
+            let todo = [];
+            data.forEach(doc => {
+                todo.push(doc.data());
+            });
+            return res.json(todo);
+        })
+        .catch(err => console.error(err));
 });
+
+app.post('/todo', (req, res) => {
+    const newTodo = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    };
+
+    admin
+        .firestore()
+        .collection('todo')
+        .add(newTodo)
+        .then((doc) => {
+            res.json({ message: `document ${doc.id} created successfully` });
+        })
+        .catch((err) => {
+            res.status(500).json({ error: 'something went wrong' });
+            console.error(err);
+        });
+});
+
+exports.api = functions.https.onRequest(app);
