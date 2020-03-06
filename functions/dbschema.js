467becab95785742cefb7d706ccdfe298d35ce58
nikolaskokhno <kokhno.mm@gmail.com>
2020-03-06 21:52:44 +0200

    Create POST route Signup user / create function to get user unigue token

---
+++ functions/dbschema.js
@@ -0,0 +1,9 @@
+let db = {
+    todo: [
+        {
+            userHandle: 'user',
+            body: 'this is the todo body',
+            createdAt: '2020-03-06T19:01:23.588Z'
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -3,18 +3,38 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
+var config = {
+    apiKey: "AIzaSyBCcJrvlEdW3s_DcAP22HqtbojTLCClnes",
+    authDomain: "todo-server-a7d16.firebaseapp.com",
+    databaseURL: "https://todo-server-a7d16.firebaseio.com",
+    projectId: "todo-server-a7d16",
+    storageBucket: "todo-server-a7d16.appspot.com",
+    messagingSenderId: "612164216533",
+    appId: "1:612164216533:web:99229e52f41f4857f1e45c"
+  };
+
 const express = require('express');
 const app = express();
 
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
+const db = admin.firestore();
+
 app.get('/todo', (req, res) => {
-    admin
-        .firestore()
+    db
         .collection('todo')
+        .orderBy('createdAt', 'desc')
         .get()
         .then(data => {
             let todo = [];
             data.forEach(doc => {
-                todo.push(doc.data());
+                todo.push({ 
+                    todoId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                 });
             });
             return res.json(todo);
         })
@@ -25,11 +45,10 @@ app.post('/todo', (req, res) => {
     const newTodo = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
-    admin
-        .firestore()
+    db
         .collection('todo')
         .add(newTodo)
         .then((doc) => {
@@ -39,6 +58,37 @@ app.post('/todo', (req, res) => {
             res.status(500).json({ error: 'something went wrong' });
             console.error(err);
         });
+});
+
+// Signup route
+app.post('/singup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle
+    };
+
+    db.doc(`/users/${newUser.handle}`).get()
+        .then(doc => {
+            if (doc.exists) {
+                return res.status(400).json({ handle: 'this handle is already taken' });
+            } else {
+                return firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+            }
+        })
+        .then(data => {
+            return data.user.getIdToken()
+        })
+        .then(token => {
+            return res.status(201).json({ token });
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        })
 });
 
 exports.api = functions.https.onRequest(app);
